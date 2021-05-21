d5b5dc0f845be09e94cac5819d35e6eac1f27e0c
Anthony Fojas <anthony_fojas@claris.com>
2021-05-20 17:26:21 -0700

    Add linting and scripts

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    es6: true
+  },
+  extends: [
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    'no-trailing-spaces': [
+      'error',
+      {
+        ignoreComments: true,
+        skipBlankLines: true
+      }],
+    'comma-dangle': ['error', 'never'],
+    'eol-last': ['error', 'always']
+  }
+};
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('message', (msg) => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
