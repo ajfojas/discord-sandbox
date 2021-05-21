4c27261239c163d72b84bda265f77f5a4e245e43
Anthony Fojas <anthony_fojas@claris.com>
2021-05-21 13:52:01 -0700

    Refactor promises to async/await

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
   console.log(`${client.user.tag} is on :)`);
 });
 
-client.on('message', (message) => {
+client.on('message', async (message) => {
   // Commands
   if (message.content.startsWith('>')) {
     const content = message.content.toUpperCase();
@@ -17,32 +17,30 @@ client.on('message', (message) => {
     switch (command) {
       case '2WEEKS':
         if (args.length <= 0) {
-          message
-            .reply('you forgot to mention the game, dummy :P')
-            .catch((error) => {
-              console.log('MESSAGE REPLY ERROR 1 :>> ', error);
-            });
+          try {
+            await message.reply('you forgot to mention the game, dummy :P');
+          } catch (error) {
+            console.log('MESSAGE REPLY ERROR 1 :>> ', error);
+          }
           break;
         }
 
-        message.channel
-          .send(`${message.author} promises to play **${args.join(' ')}** in 2 weeks if y'all are still playing it!`)
-          .then(() => {
-            setTimeout(() => {
-              message.channel.send(`2 weeks have passed!! ${message.author}, you promised to play **${args.join(' ')}** if people are still playing it!`);
-            }, TWO_WEEKS);
-          })
-          .catch((error) => {
-            console.log('MESSAGE CHANNEL SEND ERROR 1 :>> ', error);
-          });
+        try {
+          await message.channel.send(`${message.author} promises to play **${args.join(' ')}** in 2 weeks if y'all are still playing it!`);
+          setTimeout(() => {
+            message.channel.send(`2 weeks have passed!! ${message.author}, you promised to play **${args.join(' ')}** if people are still playing it!`);
+          }, TWO_WEEKS / 100000 / 2);
+        } catch (error) {
+          console.log('MESSAGE CHANNEL SEND ERROR 1 :>> ', error);
+        }
         break;
 
       default:
-        message
-          .reply(`Sorry, \`${message.content.split(' ')[0]}\` is currently not a command :(`)
-          .catch((error) => {
-            console.log('MESSAGE REPLY ERROR 2 :>> ', error);
-          });
+        try {
+          await message.channel.send(`Sorry, \`${message.content.split(' ')[0]}\` is currently not a command :(`);
+        } catch (error) {
+          console.log('MESSAGE REPLY ERROR 2 :>> ', error);
+        }
     }
   }
 });
