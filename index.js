4379acd50198ad12027e24b7e35662a2bbddbc76
Anthony Fojas <anthony_fojas@claris.com>
2021-05-26 02:30:01 -0700

    Add LISTEN case and update linting, logic, errors, and settimeout

--- index.js
+++ index.js
@@ -16,30 +16,52 @@ client.on('message', async (message) => {
     
     switch (command) {
       case '2WEEKS':
-        if (args.length <= 0) {
+        if (args.length === 0) {
           try {
             await message.reply('you forgot to mention the game, dummy :P');
           } catch (error) {
-            console.log('MESSAGE REPLY ERROR 1 :>> ', error);
+            console.log('2WEEKS ERROR 1 :>> ', error);
           }
           break;
         }
 
         try {
           await message.channel.send(`${message.author} promises to play **${args.join(' ')}** in 2 weeks if y'all are still playing it!`);
-          setTimeout(() => {
-            message.channel.send(`2 weeks have passed!! ${message.author}, you promised to play **${args.join(' ')}** if people are still playing it!`);
-          }, TWO_WEEKS / 100000 / 2);
+          setTimeout(async () => {
+            await message.channel.send(`2 weeks have passed!! ${message.author}, you promised to play **${args.join(' ')}** if people are still playing it!`);
+          }, TWO_WEEKS);
         } catch (error) {
-          console.log('MESSAGE CHANNEL SEND ERROR 1 :>> ', error);
+          console.log('2WEEKS ERROR 2 :>> ', error);
         }
         break;
 
+      case 'LISTEN': {
+        const sourceVoiceChannel = message.member.voice.channel;
+        if (!sourceVoiceChannel) {
+          try {
+            await message.reply('you fool. Do not even attempt to summon me unless you are in a voice channel >:(');
+          } catch (error) {
+            console.log('LISTEN ERROR 1 :>> ', error);
+          }
+          break;
+        }
+
+        const botAvailable = !client.voice.connections.some((connection) => connection.channel.id === sourceVoiceChannel.id);
+        if (botAvailable) {
+          try {
+            await sourceVoiceChannel.join();
+          } catch (error) {
+            console.log('LISTEN ERROR 2 :>> ', error);
+          }
+        }
+        break;
+      }
+      
       default:
         try {
           await message.channel.send(`Sorry, \`${message.content.split(' ')[0]}\` is currently not a command :(`);
         } catch (error) {
-          console.log('MESSAGE REPLY ERROR 2 :>> ', error);
+          console.log('DEFAULT ERROR 1 :>> ', error);
         }
     }
   }
